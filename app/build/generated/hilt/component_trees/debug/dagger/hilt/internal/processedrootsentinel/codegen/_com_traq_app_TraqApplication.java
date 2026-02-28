86ce4ef5cf7c1ea81175657c812c7750729e1e77
Semal Patel <9878547+semalPatel@users.noreply.github.com>
2026-02-27 16:09:47 -0800

    chore: fix gitignore, remove tracked build artifacts

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_traq_app_TraqApplication.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.traq.app.TraqApplication"
-)
-public final class _com_traq_app_TraqApplication {
-}
